930bd4d690e70b698dce8747b33852453b69825c
Zerong Zheng <zrzheng1995@foxmail.com>
2020-05-26 22:19:52 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,8 @@ pip install --user tqdm
 pip install --user opencv-python
 pip install --user h5py
 
-luarocks install hdf5
+# luarocks install hdf5
+git clone https://github.com/anibali/torch-hdf5.git
+cd torch-hdf5
+git checkout hdf5-1.10 
+luarocks make hdf5-0-0.rockspec
